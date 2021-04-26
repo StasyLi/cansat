bb71c8eb4beb6de4b319aeb79d6ee877ca7ae2d4
StasyLi <68120957+StasyLi@users.noreply.github.com>
2021-04-26 16:20:31 +0300

    золотыми руками Даниила Александровича

---
+++ MPU6050_a.ino
@@ -0,0 +1,187 @@
+// I2C device class (I2Cdev) demonstration Arduino sketch for MPU6050 class using DMP (MotionApps v2.0)
+// 6/21/2012 by Jeff Rowberg <jeff@rowberg.net>
+/* ============================================
+I2Cdev device library code is placed under the MIT license
+Copyright (c) 2012 Jeff Rowberg
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+===============================================
+*/
+
+#include <DHT.h>
+#include <DHT_U.h>
+
+#include "I2Cdev.h"
+
+#include "MPU6050_6Axis_MotionApps20.h"
+#include "MPU6050.h"
+
+// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
+// is used in I2Cdev.h
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
+
+
+/* =========================================================================
+   NOTE: In addition to connection 3.3v, GND, SDA, and SCL, this sketch
+   depends on the MPU-6050's INT pin being connected to the Arduino's
+   external interrupt #0 pin. On the Arduino Uno and Mega 2560, this is
+   digital I/O pin 2.
+ * ========================================================================= */
+/* =========================================================================
+   NOTE: Arduino v1.0.1 with the Leonardo board generates a compile error
+   when using Serial.write(buf, len). The Teapot output uses this method.
+   The solution requires a modification to the Arduino USBAPI.h file, which
+   is fortunately simple, but annoying. This will be fixed in the next IDE
+   release. For more info, see these links:
+
+   http://arduino.cc/forum/index.php/topic,109987.0.html
+   http://code.google.com/p/arduino/issues/detail?id=958
+ * ========================================================================= */
+
+//#define OUTPUT_READABLE_QUATERNION
+//#define OUTPUT_READABLE_EULER
+//#define OUTPUT_READABLE_YAWPITCHROLL
+//#define OUTPUT_READABLE_REALACCEL
+//#define OUTPUT_READABLE_WORLDACCEL
+#define OUTPUT_TEAPOT
+
+#define DHTPIN  3 
+#define DHTTYPE DHT22
+#define LED_PIN 13       // (Arduino is 13, Teensy is 11, Teensy++ is 6)
+
+bool blinkState = false;
+
+// MPU control/status vars
+bool dmpReady = false;   // set true if DMP init was successful
+uint8_t  mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t  devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;     // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;      // count of all bytes currently in FIFO
+uint8_t  fifoBuffer[64]; // FIFO storage buffer
+
+DHT dht(DHTPIN, DHTTYPE);
+MPU6050 mpu;
+// [w, x, y, z] quaternion container
+Quaternion  q;           
+// packet structure for InvenSense teapot demo
+uint8_t teapotPacket[16] = { '$', 0x02, 0,0, 0,0, 0,0, 0,0, 0,0, 0x00,0x00, '\r', '\n' };
+
+
+// ================================================================
+// ===                      INITIAL SETUP                       ===
+// ================================================================
+
+void setup() {
+    dht.begin();
+  
+    // join I2C bus (I2Cdev library doesn't do this automatically)
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+        Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+
+    Serial.begin(115200);
+    while (!Serial); // wait for Leonardo enumeration, others continue immediately
+
+    // initialize device
+    Serial.println(F("Initializing I2C devices..."));
+    mpu.initialize();
+
+    // verify connection
+    Serial.println(F("Testing device connections..."));
+    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+
+    // wait for ready
+    Serial.println(F("\nSend any character to begin DMP programming and demo: "));
+    while (Serial.available() && Serial.read()); // empty buffer
+    while (!Serial.available());                 // wait for data
+    while (Serial.available() && Serial.read()); // empty buffer again
+
+    // load and configure the DMP
+    Serial.println(F("Initializing DMP..."));
+    devStatus = mpu.dmpInitialize();
+
+    // supply your own gyro offsets here, scaled for min sensitivity
+    mpu.setXGyroOffset(220);
+    mpu.setYGyroOffset(76);
+    mpu.setZGyroOffset(-85);
+    mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+    // make sure it worked (returns 0 if so)
+    if (devStatus == 0) {
+        // Calibration Time: generate offsets and calibrate our MPU6050
+        mpu.CalibrateAccel(8);
+        mpu.CalibrateGyro(8);
+        mpu.PrintActiveOffsets();
+        // turn on the DMP, now that it's ready
+        Serial.println(F("Enabling DMP..."));
+        mpu.setDMPEnabled(true);
+
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    } else {
+        // ERROR!
+        // 1 = initial memory load failed
+        // 2 = DMP configuration updates failed
+        // (if it's going to break, usually the code will be 1)
+        Serial.print(F("DMP Initialization failed (code "));
+        Serial.print(devStatus);
+        Serial.println(F(")"));
+    }
+
+    // configure LED for output
+    pinMode(LED_PIN, OUTPUT);
+}
+
+
+// ================================================================
+// ===                    MAIN PROGRAM LOOP                     ===
+// ================================================================
+
+void loop() {
+    // read a packet from FIFO
+    if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) { // Get the Latest packet 
+       #ifdef OUTPUT_TEAPOT
+            // display quaternion values in InvenSense Teapot demo format:
+            teapotPacket[2] = fifoBuffer[0];
+            teapotPacket[3] = fifoBuffer[1];
+            teapotPacket[4] = fifoBuffer[4];
+            teapotPacket[5] = fifoBuffer[5];
+            teapotPacket[6] = fifoBuffer[8];
+            teapotPacket[7] = fifoBuffer[9];
+            teapotPacket[8] = fifoBuffer[12];
+            teapotPacket[9] = fifoBuffer[13];
+
+            teapotPacket[10] = (dht.readTemperature() * 5.0f); 
+            teapotPacket[11] = (dht.readHumidity() * 5.0f);
+            
+            Serial.write(teapotPacket, 16);
+            //teapotPacket[13]++; // packetCount, loops at 0xFF on purpose
+        #endif
+
+        // blink LED to indicate activity
+        blinkState = !blinkState;
+        digitalWrite(LED_PIN, blinkState);
+    }
+
+    delay(10);
+}
---
+++ MPUTeapot.pde
@@ -0,0 +1,204 @@
+// I2C device class (I2Cdev) demonstration Processing sketch for MPU6050 DMP output
+// 6/20/2012 by Jeff Rowberg <jeff@rowberg.net>
+
+/* ============================================
+I2Cdev device library code is placed under the MIT license
+Copyright (c) 2012 Jeff Rowberg
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+===============================================
+*/
+
+import processing.serial.*;
+import processing.opengl.*;
+import toxi.geom.*;
+import toxi.processing.*;
+
+ToxiclibsSupport gfx;
+
+Serial port;                         // The serial port
+char[] teapotPacket = new char[16];  // InvenSense Teapot packet
+int serialCount = 0;                 // current packet byte position
+int synced = 0;
+int interval = 0;
+
+float[] q = new float[4];
+Quaternion quat = new Quaternion(1, 0, 0, 0);
+
+float temperature, humidity;
+
+void setup() {
+    size(800, 800, OPENGL);
+    gfx = new ToxiclibsSupport(this);
+
+    lights();
+    smooth();
+  
+    // display serial port list for debugging/clarity
+    println(Serial.list());
+
+    //String portName = Serial.list()[0];
+    String portName = "COM4";
+    
+    port = new Serial(this, portName, 115200);
+    port.write('r');
+}
+
+void draw() {    
+    background(0);
+    
+    noStroke();
+    textSize(30);
+    textAlign(LEFT, DOWN);
+    fill(255);
+    
+    text("TEMP "+str(temperature)+"°C", width*0.7f, height*0.1f);
+    text("HMD " +str(humidity)+" %",   width*0.71f, height*0.15f);
+    
+    pushMatrix();
+    pushStyle();
+    translate(width/2, height/2);
+    scale(2);
+
+    // toxiclibs direct angle/axis rotation from quaternion (NO gimbal lock!)
+    // (axis order [1, 3, 2] and inversion [-1, +1, +1] is a consequence of
+    // different coordinate system orientation assumptions between Processing
+    // and InvenSense DMP)
+    float[] axis = quat.toAxisAngle();
+    rotate(-axis[0], -axis[1], axis[3], -axis[2]);
+
+    fill(255, 0, 0, 200);
+    box(10, 10, 200);
+    
+    fill(0, 0, 255, 200);
+    pushMatrix();
+    translate(0, 0, -120);
+    rotateX(PI/2);
+    drawCylinder(0, 20, 20, 8);
+    popMatrix();
+   
+    fill(0, 255, 0, 200);
+    beginShape(TRIANGLES);
+    vertex(-100,  2, 30); vertex(0,  2, -80); vertex(100,  2, 30);  // wing top layer
+    vertex(-100, -2, 30); vertex(0, -2, -80); vertex(100, -2, 30);  // wing bottom layer
+    vertex(-2, 0, 98); vertex(-2, -30, 98); vertex(-2, 0, 70);  // tail left layer
+    vertex( 2, 0, 98); vertex( 2, -30, 98); vertex( 2, 0, 70);  // tail right layer
+    endShape();
+    beginShape(QUADS);
+    vertex(-100, 2, 30); vertex(-100, -2, 30); vertex(  0, -2, -80); vertex(  0, 2, -80);
+    vertex( 100, 2, 30); vertex( 100, -2, 30); vertex(  0, -2, -80); vertex(  0, 2, -80);
+    vertex(-100, 2, 30); vertex(-100, -2, 30); vertex(100, -2,  30); vertex(100, 2,  30);
+    vertex(-2,   0, 98); vertex(2,   0, 98); vertex(2, -30, 98); vertex(-2, -30, 98);
+    vertex(-2,   0, 98); vertex(2,   0, 98); vertex(2,   0, 70); vertex(-2,   0, 70);
+    vertex(-2, -30, 98); vertex(2, -30, 98); vertex(2,   0, 70); vertex(-2,   0, 70);
+    endShape();
+    
+    popStyle();
+    popMatrix();
+    
+    if (millis() - interval > 1000) {
+        port.write('r');
+        println(str(millis())+" ALERT");
+        
+        fill(255, 0, 0);
+        textSize(70);
+        textAlign(CENTER, CENTER);
+        text("NO CONNECTION", width/2, height*0.7f);
+    }
+}
+
+void serialEvent(Serial port) {
+    while (port.available() > 0) {
+        int ch = port.read();
+
+        if (synced == 0 && ch != '$') return;   // initial synchronization - also used to resync/realign if needed
+        synced = 1;
+        //print((char)ch);
+
+        if ((serialCount == 1 && ch != 2)
+            || (serialCount == 14 && ch != '\r')
+            || (serialCount == 15 && ch != '\n'))  {
+            serialCount = 0;
+            synced = 0;
+            return;
+        }
+        
+        interval = millis();
+
+        if (serialCount > 0 || ch == '$') {
+            teapotPacket[serialCount++] = (char)ch;
+            if (serialCount == 16) {
+                serialCount = 0; // restart packet byte position
+                
+                // get quaternion from data packet
+                q[0] = ((teapotPacket[2] << 8) | teapotPacket[3]) / 16384.0f;
+                q[1] = ((teapotPacket[4] << 8) | teapotPacket[5]) / 16384.0f;
+                q[2] = ((teapotPacket[6] << 8) | teapotPacket[7]) / 16384.0f;
+                q[3] = ((teapotPacket[8] << 8) | teapotPacket[9]) / 16384.0f;
+                for (int i = 0; i < 4; i++) if (q[i] >= 2) q[i] -= 4;
+                
+                temperature = teapotPacket[10] / 5.0f;
+                humidity    = teapotPacket[11] / 5.0f;
+                
+                // set our toxilibs quaternion to new data
+                quat.set(q[0], q[1], q[2], q[3]);
+            }
+        }
+    }
+}
+
+void drawCylinder(float topRadius, float bottomRadius, float tall, int sides) {
+    float angle = 0;
+    float angleIncrement = TWO_PI / sides;
+    beginShape(QUAD_STRIP);
+    for (int i = 0; i < sides + 1; ++i) {
+        vertex(topRadius*cos(angle), 0, topRadius*sin(angle));
+        vertex(bottomRadius*cos(angle), tall, bottomRadius*sin(angle));
+        angle += angleIncrement;
+    }
+    endShape();
+    
+    // If it is not a cone, draw the circular top cap
+    if (topRadius != 0) {
+        angle = 0;
+        beginShape(TRIANGLE_FAN);
+        
+        // Center point
+        vertex(0, 0, 0);
+        for (int i = 0; i < sides + 1; i++) {
+            vertex(topRadius * cos(angle), 0, topRadius * sin(angle));
+            angle += angleIncrement;
+        }
+        endShape();
+    }
+  
+    // If it is not a cone, draw the circular bottom cap
+    if (bottomRadius != 0) {
+        angle = 0;
+        beginShape(TRIANGLE_FAN);
+    
+        // Center point
+        vertex(0, tall, 0);
+        for (int i = 0; i < sides + 1; i++) {
+            vertex(bottomRadius * cos(angle), tall, bottomRadius * sin(angle));
+            angle += angleIncrement;
+        }
+        endShape();
+    }
+}
